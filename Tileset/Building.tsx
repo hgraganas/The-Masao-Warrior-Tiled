42731b403de70f74e9ca7f338e02204a2620a7e8
RC Pelisco <rcpelisco@gmail.com>
2017-08-24 11:25:37 +0800

    Added stuff

--- Tileset/Building.tsx
+++ Tileset/Building.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Building" tilewidth="16" tileheight="16" tilecount="1482" columns="39">
- <image source="../Source/Building.png" width="625" height="608"/>
+<tileset name="Building" tilewidth="16" tileheight="16" tilecount="2204" columns="58">
+ <image source="../Source/Building.png" width="939" height="608"/>
 </tileset>
