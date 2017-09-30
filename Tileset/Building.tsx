44be39c5c3ada0b842d0d69e7a1bb9d8a7ef6c91
Honey Grace Raganas <hgraganas@gmail.com>
2017-09-30 20:04:33 +0800

    Hi nog pag pull una

--- Tileset/Building.tsx
+++ Tileset/Building.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Building" tilewidth="16" tileheight="16" tilecount="2204" columns="58">
- <image source="../Source/Building.png" width="939" height="608"/>
+ <image source="../Source/Building.png" width="928" height="608"/>
  <tile id="71">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
